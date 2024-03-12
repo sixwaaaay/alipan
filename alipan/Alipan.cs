ddd86397db3c0dde413289f5320f9280dd50f558
sixwaaaay <lw.2675@qq.com>
2024-03-12 13:18:36 +0800

    feat: client

---
+++ alipan/Alipan.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http.Headers;
+
+namespace alipan;
+
+public class Alipan
+{
+    private readonly HttpClient _httpClient = new()
+    {
+        BaseAddress = new Uri("https://open.aliyundrive.com")
+    };
+
+    public string AccessToken
+    {
+        set => _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", value);
+    }
+
+    public UserInfo User { get; }
+    
+    public Driver Driver { get; }
+
+    public Alipan()
+    {
+        User = new UserInfo(_httpClient);
+        Driver = new Driver(_httpClient);
+    }
+}
